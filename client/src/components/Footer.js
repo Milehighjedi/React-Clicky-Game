b5b136025b3497f0cc7520cb100809d9d7a89296
Luke Kwerneland <lkwerneland@gmail.com>
2018-07-23 18:06:22 -0600

    new push

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer bg-info">
+    <div className="container">
+      <span className="text-dark">
+        Clicky Game - 100% of your daily Greens!
+      </span>
+    </div>
+  </footer>
+);
+export default Footer;
---
+++ client/src/components/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Main = props => (
+  <div className="container text-center">{props.children}</div>
+);
+
+export default Main;
